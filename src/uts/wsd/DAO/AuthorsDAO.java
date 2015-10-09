a1362eae1d53524cd51da08c3bceeb426fdb6833
Harold He <haroldfrost1@hotmail.com>
2015-10-09 13:30:34 +1100

    first commit

---
+++ src/uts/wsd/DAO/AuthorsDAO.java
@@ -0,0 +1,88 @@
+package uts.wsd.DAO;
+
+
+
+import java.io.*;
+
+
+import javax.xml.bind.*;
+
+import uts.wsd.domain.*;
+
+public class AuthorsDAO {
+	/*
+	 * Data Access Object class for
+	 * Authors
+	 */
+	
+	
+	private String filePath;
+	private Authors authors;
+	
+	public AuthorsDAO() {
+		// init with setting filePath to
+		// "authors.xml"
+		setFilePath("authors.xml");
+		authors = new Authors();
+		readAuthors();
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+			
+	public void save(){
+		try{
+			JAXBContext jc = JAXBContext.newInstance(Authors.class);
+			Marshaller m = jc.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			m.marshal(authors, new FileOutputStream(filePath));
+		}
+		catch (JAXBException e) {
+			e.printStackTrace();
+		}	
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readAuthors(){
+		JAXBContext jc;
+		try{
+			// ready unmarshaller
+			jc = JAXBContext.newInstance(Authors.class);
+			Unmarshaller un = jc.createUnmarshaller();
+			
+			// Unmarshall from authors.xml
+			FileInputStream fin = new FileInputStream(filePath);
+			authors = (Authors)un.unmarshal(fin);
+			
+			fin.close();
+		}
+		catch (JAXBException e){
+			e.printStackTrace();
+		}
+		catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
+	
+//	We don't need this function yet 
+//	
+//	public void addAuthor(Author author){
+//		
+//		readAuthors();
+//		
+//		// Setting author id with current largest id +1
+//		author.setId(authors.getLargestId() + 1);
+//		
+//		//add & save
+//		authors.addAuthor(author);
+//		save();
+//	}
+
+}
